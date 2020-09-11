70678821463ae1d9a664df2290180807d94d2e6c
Zubair Shaikh <zubairify@gmail.com>
2020-09-11 17:56:39 +0530

    Eclipse maven project

---
+++ MavenPro/src/main/java/com/lti/code/Arithmetic.java
@@ -0,0 +1,8 @@
+package com.lti.code;
+
+public class Arithmetic {
+
+	public int sum(int a, int b) {
+		return a+b;
+	}
+}
---
+++ MavenPro/src/test/java/com/lti/test/TestArithmetic.java
@@ -0,0 +1,22 @@
+package com.lti.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lti.code.Arithmetic;
+
+public class TestArithmetic {
+	private Arithmetic arth;
+	
+	@Before
+	public void init() {
+		arth = new Arithmetic();
+	}
+	
+	@Test
+	public void testSum() {
+		assertEquals(10, arth.sum(4, 6));
+	}
+}
